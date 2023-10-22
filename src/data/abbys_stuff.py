a230ab0b292e7240ad7b6c9ed79b2bd8a34481a4
Abby <a.catherine.perkins@gmail.com>
2023-10-22 11:42:28 -0500

    updated

--- src/data/abbys_stuff.py
+++ src/data/abbys_stuff.py
@@ -1,3 +1,11 @@
 # all meteorological data downloaded from below website to maintain consistency with AP-42
 # https://www1.ncdc.noaa.gov/pub/data/nsrdb-solar/summary-stats-2010/dailystats/
+import pandas
+import pandas as pd
+from pathlib import Path
 
+DIRECTORY = Path('C:/Users/Abby/PycharmProjects/rapid-tanks')
+data = pd.read_csv(DIRECTORY / 'met/NSRDB_DailyStatistics_19910101_20101231_700260.txt',
+                   skiprows=[0, 1], delim_whitespace=True)
+data = data.loc[12, ]
+print(data)
